b01b9b9a0e2b0ec15fd771efc01193b7465e509e
sqweeq <tsai8@hotmail.com>
2021-08-15 04:42:20 +0800

    changes

--- src/test/java/TestCalc.java
+++ src/test/java/TestCalc.java
@@ -1,12 +1,14 @@
-import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class TestCalc extends TestCase {
-
+public class TestCalc {
+	@Test
 	public void testCalc() {
 		assertEquals(3, Calc.add(2, 1));
 		System.out.println(Calc.add(2, 1));
